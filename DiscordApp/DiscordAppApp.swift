ee0560487a3eb451a6c5aed26b83015b68bc4e5d
Gulchin Ibrahimli <ibragimli.gulchin@gmail.com>
2024-10-29 09:59:45 +0100

    Initial Commit

---
+++ DiscordApp/DiscordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordAppApp.swift
+//  DiscordApp
+//
+//  Created by Gulchin Ibragimli on 29.10.24.
+//
+
+import SwiftUI
+
+@main
+struct DiscordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
